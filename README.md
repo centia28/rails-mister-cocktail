bff59273ba8993a6af3e2f862936f47d8bdf766c
TCHALLA Nina Essowaza <nina.tchalla@gmail.com>
2019-08-08 15:25:25 +0200

    Basic features of cocktails

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## Seeds
 - Add ingredients (done)
 - Import ingredients from json list
+
+## Features
+- List cocktails (done)
+- Create a new cocktail (done)
+- See details of a cocktails: each ingerdients and the doses(done)
+- Add a new dose (ingredient/description pair) to an existing cocktail
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+  get 'doses/create'
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    member do
+      resources :doses, only: [:new, :create]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
