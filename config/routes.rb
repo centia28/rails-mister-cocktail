53152aee5dac6d907881c17f51b6497aa14aa1ec
TCHALLA Nina Essowaza <nina.tchalla@gmail.com>
2019-08-08 16:04:25 +0200

    Features for doses done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
   resources :cocktails, only: [:index, :show, :new, :create] do
     member do
       resources :doses, only: [:new, :create]
     end
   end
+  delete 'doses/:id', to: 'doses#destroy', as: 'delete_dose'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
